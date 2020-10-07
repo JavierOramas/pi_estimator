c477db1cf4c5be3e63032eabb204142a69b48e31
Javier Oramas <javiale2000@gmail.com>
2020-10-06 22:09:04 -0400

    draw select points

--- estimator.py
+++ estimator.py
@@ -1,9 +1,37 @@
 from random import random as rdn
+import matplotlib.pyplot as plt
+import streamlit as st
+from math import sqrt
 
 def estimate_pi(num_points:int):
-    points = []
+    
+    points_x = []
+    points_y = []
+    inside_points = []
+    
     for i in range(num_points):
-        points.append((rdn(),rdn()))
-    print(points)
+        x = rdn()
+        y = rdn()
+        points_x.append(x)
+        points_y.append(y)
+        
+        if sqrt(x**2+y**2) < 1:
+            inside_points.append((x,y))
+        
+    
+    circle = plt.Circle((0,0),1,color='blue')
+    fig, ax = plt.subplots()
+    ax.add_artist(circle)
+    
+    for i in range(len(points_x)):
+        if ((points_x[i]),(points_y[i])) in inside_points:
+            ax.plot((points_x[i]),(points_y[i]),'o', color='red')
+        else :
+            ax.plot((points_x[i]),(points_y[i]),'o', color='black')
+            
+    st.pyplot(plt.show())
 
-estimate_pi(2)       
+    
+num = st.number_input('Number of Points', value=1.)    
+if st.button('Estimate'):
+    estimate_pi(int(num))
